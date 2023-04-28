7e9fcbad3076574de60fd02cce9b8b10b063fae9
bekhzod-olimov <bekhzod.olimov@gmail.com>
2023-04-28 09:49:57 +0900

    fixing bugs

--- .ipynb_checkpoints/download_data-checkpoint.py
+++
@@ -1,8 +0,0 @@
-# Import library 
-import opendatasets as od
-
-# Set dataset link
-dataset = "https://www.kaggle.com/datasets/andrewmvd/road-sign-detection"
-
-# Download dataset
-od.download(dataset)
